fc3f86edd703f8f5fc3113600bd0409782135d56
sreedev <xreedev@gmail.com>
2024-09-03 23:45:34 +0530

    File restructure

--- SchoolSystem/SchoolSystem/src/main/java/com/simplogics/SchoolSystem/Repository/ITeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.simplogics.SchoolSystem.Repository;
-
-import com.simplogics.SchoolSystem.Model.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITeacherRepository extends JpaRepository<Teacher,Integer> {
-}
