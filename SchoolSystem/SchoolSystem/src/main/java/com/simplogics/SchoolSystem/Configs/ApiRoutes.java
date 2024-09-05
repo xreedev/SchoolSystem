395ee7cb6b497aa49ad5172c1de31e04a760d3ad
sreedev <xreedev@gmail.com>
2024-09-05 21:48:36 +0530

    Restructure files

--- SchoolSystem/SchoolSystem/src/main/java/com/simplogics/SchoolSystem/Configs/ApiRoutes.java
+++
@@ -1,6 +0,0 @@
-package com.simplogics.SchoolSystem.Configs;
-
-public class ApiRoutes {
-    public static final String ID="/{id}";
-    public static final String STUDENT="/students";
-}
--- SchoolSystem/SchoolSystem/src/main/java/com/simplogics/SchoolSystem/Repository/IStudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.simplogics.SchoolSystem.Repository;
-
-import com.simplogics.SchoolSystem.Model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IStudentRepository extends JpaRepository<Student,Integer> {
-}
